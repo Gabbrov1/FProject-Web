2c142e438a19ca14b475d73646e217e5a437b1e2
Gabbrov1 <ionut4965@gmail.com>
2026-02-01 11:52:31 +0000

    basic setup change

---
+++ BackEnd/app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
